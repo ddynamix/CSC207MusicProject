8019b027eb78ad4d7a2598127805eafa5f382775
tasnimreza <tasnim.reza@mail.utoronto.ca>
2024-07-16 17:37:36 -0400

    updated the db connection
    git push origin main

---
+++ src/main/java/dataaccess/mongodb/testObj.java
@@ -0,0 +1,23 @@
+public class testObj{
+    private String memberID = "";
+    private int timer = 0;
+    private int xp = 0;
+    public testObj(String memberID, int timer, int xp){
+    }
+    public String getMemberID() {
+        return memberID;
+    }
+    public void setMemberID(String memberID) {
+        this.memberID = memberID;
+    }
+    public void getxp(){
+        return xp;
+    }
+    public int getTimer() {
+        return timer;
+    }
+    public void setTimer(int timer) {
+        this.timer = timer;
+    }
+
+}
