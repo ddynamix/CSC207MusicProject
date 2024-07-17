a7a63a184c6e2b6945ca1226d9e81e010c0faad7
timkoukarine <tim.koukarine@gmail.com>
2024-07-16 21:38:08 -0400

    work on user dataaccessobject

--- src/main/java/dataaccess/UserDataAccessObject.java
+++ src/main/java/dataaccess/UserDataAccessObject.java
@@ -1,8 +1,17 @@
 package dataaccess;
 
-import dataaccess.mongodb.Connection;
-import entity.user.User;
-import entity.user.UserFactory;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertOneResult;
+import entity.user.*;
+import org.bson.Document;
 
 public class UserDataAccessObject implements UserDataAccessInterface {
 
@@ -10,13 +19,35 @@ public class UserDataAccessObject implements UserDataAccessInterface {
 
     }
 
-    private Connection connection;
+    private MongoClient mongoClient;
     private UserFactory userFactory;
+    private MongoDatabase mongoDatabase;
+    private MongoCollection mongoCollection;
+    private User user;
 
-    public UserDataAccessObject(Connection connection, UserFactory userFactory) {
-        this.userFactory = userFactory;
-        this.connection = connection;
+    public UserDataAccessObject(User user) {
+        this.user= user;
+        String connectionString = "mongodb+srv://tasnimreza:dbtestpass@cluster0.vlnfmzu.mongodb.net/?appName=Cluster0";
 
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        this.mongoClient = MongoClients.create("mongodb+srv://tasnimreza:dbtestpass@cluster0.vlnfmzu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        this.mongoDatabase = mongoClient.getDatabase("userDataBase");
+
+        if (user instanceof ArtistUser) {
+            this.mongoCollection = mongoDatabase.getCollection("artistUser");
+        } else if (user instanceof AudienceUser) {
+            this.mongoCollection = mongoDatabase.getCollection("audienceUser");
+        } else if (user instanceof VenueUser) {
+            this.mongoCollection = mongoDatabase.getCollection("venueUser");
+        }
     }
 
     @Override
@@ -29,8 +60,19 @@ public class UserDataAccessObject implements UserDataAccessInterface {
 
     }
 
-    private void create() {
-
+    private void create(String username, String password, String email, String firstName, String lastName) {
+        if (userExistsInDatabase(username)) {
+//            throw new UserNotFoundException();
+        } else {
+            user = userFactory.createUser(username, password, email, firstName, lastName);
+            Document document = new Document("username", user.getUsername())
+                    .append("password", user.getPassword())
+                    .append("email", user.getEmail())
+                    .append("firstName", user.getFirstName())
+                    .append("lastName", user.getLastName());
+            InsertOneResult insertResult = MongoCollection.insertOne(document);
+            user.setId(insertResult.getInsertedId().toString());
+        }
     }
 
     @Override
