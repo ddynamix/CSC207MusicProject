e6c0105cfe4fbad8286e282857db2689c17939bf
timkoukarine <tim.koukarine@gmail.com>
2024-07-23 20:44:13 -0400

    initial commit

--- src/main/java/dataaccess/EventDataAccessObject.java
+++ src/main/java/dataaccess/EventDataAccessObject.java
@@ -1,2 +1,49 @@
-package dataaccess;public class EventDataAccessObject {
+package dataaccess;
+
+import entity.event.Event;
+
+public class EventDataAccessObject implements EventDataAccessInterface {
+
+    public static class EventAlreadyExistsException extends Exception {
+        public EventAlreadyExistsException() {
+            super("Event already exists in the database");
+        }
+    }
+
+    public static class EventDoesntExistException extends Exception {
+        public EventDoesntExistException() {
+            super("Event does not exist in the database");
+        }
+    }
+
+    public EventDataAccessObject() {
+
+    }
+
+    @Override
+    public boolean eventExists(String eventName) {
+        return false;
+    }
+
+    @Override
+    public void createEvent(Event event) throws EventAlreadyExistsException {
+        throw new EventAlreadyExistsException();
+    }
+
+    @Override
+    public void deleteEvent(Event event) throws EventDoesntExistException {
+        throw new EventDoesntExistException();
+    }
+
+    @Override
+    public Event getEventFromTitle(String eventName) throws EventDoesntExistException {
+        Event event = new Event();
+        throw new EventDoesntExistException();
+        return event;
+
+    }
+
+    @Override
+    public Map<String, Event> getEvents() {
+    }
 }
