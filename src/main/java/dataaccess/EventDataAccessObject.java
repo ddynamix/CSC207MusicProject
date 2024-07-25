4ddba3b11f616e9c71d6ac4645fb47a8e439f5c5
timkoukarine <tim.koukarine@gmail.com>
2024-07-25 19:48:33 -0400

    event CRUDn

--- src/main/java/dataaccess/EventDataAccessObject.java
+++ src/main/java/dataaccess/EventDataAccessObject.java
@@ -1,6 +1,21 @@
 package dataaccess;
 
+import com.mongodb.client.*;
 import entity.event.Event;
+import org.bson.conversions.Bson;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.InsertOneResult;
+import entity.user.*;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import java.lang.Map;
+
+import static com.mongodb.client.model.Filters.eq;
 
 public class EventDataAccessObject implements EventDataAccessInterface {
 
@@ -16,34 +31,75 @@ public class EventDataAccessObject implements EventDataAccessInterface {
         }
     }
 
-    public EventDataAccessObject() {
+    public MongoClient mongoClient;
+    public MongoDatabase mongoDatabase;
+    public MongoCollection mongoCollection;
 
+    public EventDataAccessObject() {
+        String connectionString = "mongodb+srv://tasnimreza:dbtestpass@cluster0.vlnfmzu.mongodb.net/?appName=Cluster0";
+
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        this.mongoClient = MongoClients.create("mongodb+srv://tasnimreza:dbtestpass@cluster0.vlnfmzu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        this.mongoDatabase = mongoClient.getDatabase("contentDataBase");
+        this.mongoCollection = mongoDatabase.getCollection("eventContent");
     }
 
     @Override
     public boolean eventExists(String eventName) {
-        return false;
+        Bson filter = eq("eventName", eventName);
+        return mongoCollection.find(filter).iterator().hasNext();
     }
 
     @Override
     public void createEvent(Event event) throws EventAlreadyExistsException {
-        throw new EventAlreadyExistsException();
+        if (eventExists(event.getTitle())) {
+            throw new EventAlreadyExistsException();
+        } else{
+            Document document = new Document("eventName", event.getTitle())
+                    .append("eventDescription", event.getDescription())
+                    .append("eventDate", event.getDateAndTime())
+                    .append("eventOrganizer", event.getTags())
+                    .append("eventAttendees", event.getPostDate())
+                    .append("eventVenue", event.getVenue())
+                    .append("eventMedia", event.getAttachedMedia());
+            InsertOneResult insertResult = mongoCollection.insertOne(document);
+            event.setId(insertResult.getInsertedId().toString());
+            mongoCollection.insertOne(document);
+        }
     }
 
     @Override
     public void deleteEvent(Event event) throws EventDoesntExistException {
-        throw new EventDoesntExistException();
+        if (!eventExists(event.getTitle())) {
+            throw new EventDoesntExistException();
+        } else {
+            Bson filter = eq("eventName", event.getTitle());
+            mongoCollection.deleteOne(filter);
+        }
     }
 
     @Override
     public Event getEventFromTitle(String eventName) throws EventDoesntExistException {
-        Event event = new Event();
-        throw new EventDoesntExistException();
-        return event;
-
+        if (!eventExists(eventName)) {
+            throw new EventDoesntExistException();
+        } else {
+            Bson filter = eq("eventName", eventName);
+            MongoCursor iterator = mongoCollection.find(filter).iterator();
+            //todo convert iterator to event
+            return event;;
+        }
     }
+
     @Override
-    public Map<String, Event> getEvents() {
+    public Map<String, Event> getEvents() { //todo implement. return all events
 
     }
 }
